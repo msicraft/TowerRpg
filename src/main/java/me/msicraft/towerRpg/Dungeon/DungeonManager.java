0b605b7d014fa3e282e3a524270c82b2122158e8
msicraft <rlarua9689@gmail.com>
2024-09-10 17:37:24 +0900

    Update

--- src/main/java/me/msicraft/towerRpg/Dungeon/DungeonManager.java
+++ src/main/java/me/msicraft/towerRpg/Dungeon/DungeonManager.java
@@ -1,6 +1,10 @@
 package me.msicraft.towerRpg.Dungeon;
 
+import me.msicraft.towerRpg.Dungeon.Data.DungeonType;
 import me.msicraft.towerRpg.TowerRpg;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DungeonManager {
 
@@ -8,6 +12,40 @@ public class DungeonManager {
 
     public DungeonManager(TowerRpg plugin) {
         this.plugin = plugin;
+        reloadVariables();
+    }
+
+    public void reloadVariables() {
+        DungeonType[] dungeonTypes = DungeonType.values();
+        List<String> dungeonKeys = new ArrayList<>(dungeonTypes.length);
+        for (DungeonType dungeonType : dungeonTypes) {
+            dungeonKeys.add(dungeonType.getKey());
+        }
+
+        boolean isChange = false;
+        for (String key : dungeonKeys) {
+            String path = "Setting.Dungeon." + key;
+            try {
+                DungeonType dungeonType = DungeonType.valueOf(key.toUpperCase());
+                if (plugin.getConfig().contains(path)) {
+                    dungeonType.setDisplayName(plugin.getConfig().contains(path + ".DisplayName") ? plugin.getConfig().getString(path + ".DisplayName") : dungeonType.getDisplayName());
+                    dungeonType.setTotalFloor(plugin.getConfig().contains(path + ".TotalFloor") ? plugin.getConfig().getInt(path + ".TotalFloor") : 0);
+                    dungeonType.setMaxPlayer(plugin.getConfig().contains(path + ".MaxPlayer") ? plugin.getConfig().getInt(path + ".MaxPlayer") : 1);
+                } else {
+                    if (!isChange) {
+                        isChange = true;
+                    }
+                    plugin.getConfig().set(path + ".DisplayName", dungeonType.getDisplayName());
+                    plugin.getConfig().set(path + ".TotalFloor", dungeonType.getTotalFloor());
+                    plugin.getConfig().set(path + ".MaxPlayer", dungeonType.getMaxPlayer());
+                }
+            } catch (IllegalArgumentException ignored) {
+            }
+        }
+
+        if (isChange) {
+            plugin.saveConfig();
+        }
     }
 
 }
